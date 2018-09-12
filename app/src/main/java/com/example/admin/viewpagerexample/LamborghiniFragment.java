6de91afc6f256ba9166e2bbd89a72fbdddecf1e3
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-09-12 22:32:45 +0530

    Added another fragment.

---
+++ app/src/main/java/com/example/admin/viewpagerexample/LamborghiniFragment.java
@@ -0,0 +1,17 @@
+package com.example.admin.viewpagerexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+public class LamborghiniFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_lamborghini, container, false);
+    }
+}
